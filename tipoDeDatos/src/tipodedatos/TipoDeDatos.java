40bdbf9a08878b91e0446e5c31e38401d84d5a43
Kevin.Jimenez24 <kivito2009@hotmail.com>
2020-03-10 11:08:20 -0500

    tipo de dato

---
+++ tipoDeDatos/src/tipodedatos/TipoDeDatos.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tipodedatos;
+
+/**
+ *
+ * @author SISTEMAS CORP
+ */
+public class TipoDeDatos {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    // enteros -32768 a 32767
+    int numeroEntero = 10;
+    double numeroDouble = 3;
+    char letra = 'A';
+    String caderna = "hola mundo"; 
+    boolean valorBool = false;    
+    boolean valorBoola = false;
+    
+   
+    public static void main(String[] args) {
+        
+        // TODO code application logic here
+    }
+    
+    // comentario de una linea        
+    /*
+    asdasdasdas
+    asdasd
+    asdas
+    asdasd
+    */
+    
+}
